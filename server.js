7363da8e33908dff77f6edbdcbf42c41eb85536e
Charles Cheng <charles.yhcheng@gmail.com>
2020-11-30 10:38:58 +0000

    Imported multer to read upfile

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const app = express();
+const multer = require('multer');
+const upload = multer({ dest: './public/uploads/' });
+
 require('dotenv').config()
 
-const app = express();
-
 app.use(cors());
+
+// Mount the body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Serve static files
 app.use('/public', express.static(process.cwd() + '/public'));
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
 
 // Request Logger
 app.use((req, res, next) => {
@@ -13,14 +26,13 @@ app.use((req, res, next) => {
   next();
 })
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
-
-
-
-
+// Basic Configuration
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+// Read file metadata
+app.post("/api/fileanalyse", upload.single("upfile"), (req,res)=>{
+  console.log(req.file)
+})
